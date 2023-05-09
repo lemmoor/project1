17b94a3fd10bcd073a2879bd0a1cb9965450daad
lemmoor <49235295+lemmoor@users.noreply.github.com>
2023-05-09 21:04:03 +0200

    Vocabulary page and Scroll to top on page change

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,19 @@ import './index.css';
 import Nav from './components/Nav';
 import Dictionary from './pages/Dictionary';
 import Idioms from './pages/Idioms';
+import Vocabulary from './pages/Vocabulary';
+import ScrollToTop from './components/ScrollToTop';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dictionary" element={<Dictionary />} />
         <Route path="/idioms" element={<Idioms />} />
+        <Route path="/vocabulary" element={<Vocabulary />} />
       </Routes>
     </Router>
   </React.StrictMode>
---
+++ src/pages/Vocabulary.tsx
@@ -0,0 +1,154 @@
+function Vocabulary() {
+  return (
+    <main className="container p-4">
+      <p>
+        Slang words are a fun way to add flavor to your conversations and sound
+        like a native speaker. They are informal and often used in everyday
+        conversation. Here are some popular British and American slang words
+        that you can use.
+      </p>
+      <hr className="bg-primary h-[2px] border-0 my-2" />
+      <h2 className="text-xl mt-6 font-bold">British English</h2>
+      <hr className="bg-secondary h-[2px] border-0 my-2" />
+      <ul className="list-decimal ml-4">
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Mate</p>
+          <p>
+            British slang for a friend. Similar to &quot;dude&quot; in American
+            English.
+          </p>
+          <p className="text-info-content italic pl-2">
+            I&apos;m meeting up with my mate at the pub tonight.
+          </p>
+        </li>
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Cheeky</p>
+          <p>Slightly rude or showing no respect, but often in a funny way.</p>
+          <p className="text-info-content italic pl-2">
+            She&apos;s got such a cheeky grin.
+          </p>
+        </li>
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Bodged</p>
+          <p>Used to describe something, usually a job, that is done badly.</p>
+          <p className="text-info-content italic pl-2">
+            I tried to fix the door handle myself, but I ended up bodging it.
+          </p>
+          <p className="text-info-content italic pl-2">
+            The handyman botched the repair job, and I had to call someone else
+            to fix it.
+          </p>
+        </li>
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Gutted</p>
+          <p>Extremely disappointed or upset.</p>
+          <p className="text-info-content italic pl-2">
+            I was absolutely gutted when I found out I didn&apos;t get the job I
+            applied for.
+          </p>
+        </li>
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Quid</p>
+          <p>A slang word for &quot;pound&quot;.</p>
+          <p className="text-info-content italic pl-2">
+            I earned 50 quid from my part-time job last week.
+          </p>
+        </li>
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Knackered</p>
+          <p>Really tired, exhausted.</p>
+          <p className="text-info-content italic pl-2">
+            After working for 12 hours straight, I feel absolutely knackered.
+          </p>
+        </li>
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Dodgy</p>
+          <p>
+            Used to mean anything that&apos;s low-quality, potentially dangerous
+            or unreliable.
+          </p>
+          <p className="text-info-content italic pl-2">
+            I wouldn&apos;t trust that website, it looks pretty dodgy.
+          </p>
+        </li>
+      </ul>
+      <h2 className="text-xl mt-6 font-bold">American English</h2>
+      <hr className="bg-secondary h-[2px] border-0 my-2" />
+      <ul className="list-decimal ml-4">
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Legit</p>
+          <p>
+            Something that is good or worthwhile. Short for legitimate (meaning
+            authentic or real).
+          </p>
+          <p className="text-info-content italic pl-2">
+            That online store is legit, I&apos;ve ordered from them before and
+            got my products on time.
+          </p>
+        </li>
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Go Dutch</p>
+          <p>Every person pays for their own food/drinks.</p>
+          <p className="text-info-content italic pl-2">
+            We decided to go Dutch and split the bill for dinner.
+          </p>
+        </li>
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Zone out</p>
+          <p>
+            To get distracted and not pay attention to what is happening around
+            you.
+          </p>
+          <p className="text-info-content italic pl-2">
+            I&apos;m sorry, I zoned out during that meeting. Can you repeat what
+            you said?
+          </p>
+        </li>
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Dude</p>
+          <p>
+            A casual greeting used instead of first names. Similar to mate in
+            British English.
+          </p>
+          <p className="text-info-content italic pl-2">
+            Hey dude, how&apos;s it going?
+          </p>
+        </li>
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Curve ball</p>
+          <p>
+            Something tricky or unexpected, like trying to hit a curve ball in
+            baseball.
+          </p>
+          <p className="text-info-content italic pl-2">
+            I thought I was prepared for the exam, but the last question threw
+            me a curve ball and I had no idea how to answer it.
+          </p>
+        </li>
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Ditch</p>
+          <p>
+            To leave a place or person unexpectedly, or to not show up to prior
+            plans.
+          </p>
+          <p className="text-info-content italic pl-2">
+            The group of friends made plans to go hiking, but one of them had to
+            ditch at the last minute due to work.
+          </p>
+        </li>
+        <li className="pl-1 mt-2">
+          <p className="font-bold">Have dibs on</p>
+          <p>
+            A right to have or get something from someone, or to use something:.
+          </p>
+          <p className="text-info-content italic pl-2">
+            Don&apos;t even think about touching that last slice of pizza. I
+            have dibs on it.
+          </p>
+        </li>
+      </ul>
+    </main>
+  );
+}
+
+export default Vocabulary;
