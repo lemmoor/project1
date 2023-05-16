252dbd52383f3c2d8290e4918138d1ae8f2cfead
lemmoor <49235295+lemmoor@users.noreply.github.com>
2023-05-16 14:44:56 +0200

    test quiz1

---
+++ src/components/Quiz/quiz1.ts
@@ -0,0 +1,15 @@
+export const quiz1 = {
+    q1: [{
+        a: "text 1",
+        isCorrect: false
+    },
+    {
+        a: "text 2",
+        isCorrect: false
+    },
+    {
+        a: "text 3",
+        isCorrect: true
+    }]
+
+}
